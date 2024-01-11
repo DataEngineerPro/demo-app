507b875bb0a38d53809e60f3131b0a6bf5c5c57a
Kuldeep Desai <kuldeep.desai@qumu.com>
2024-01-11 13:15:26 +0100

    intro

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -1,6 +1,7 @@
 import { Steps } from 'intro.js-react';
 import 'intro.js/introjs.css';
 import './intro.scss';
+import { Card, ListGroup } from 'react-bootstrap';
 
 function IntroComponent(props: any) {
   console.log('props', props);
@@ -16,15 +17,14 @@ function IntroComponent(props: any) {
       position: 'left',
     },
     {
-      element: '.thumbnail-container',
+      element: '.selected-thumbnail',
       intro: 'Identify the page where the fields or tables are located',
-      position: 'right',
+      position: 'top',
     },
     {
-      element: '.konvajs-content',
       intro: CanvasIntroComponent(),
-      position: 'right',
       tooltipClass: 'customTooltip',
+      position: 'top',
     },
     {
       element: '.label-table',
@@ -55,43 +55,47 @@ function IntroComponent(props: any) {
 
 function CanvasIntroComponent() {
   return (
-    <div>
-      <div>
-        <img
-          className="img-fluid"
-          src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
-        />
-      </div>
-      <div>
-        <ul className="ollist">
-          <li className="m-0 p-0">
-            Take your cursor to the upper left corner of the relevant datapoint,
-            and drag the cursor to select the required portion.
-          </li>
-          <li className=" m-0 p-0">
-            Assign labels to each field or table. Feel free to add any optional
-            comments to specify special processing needs, such as data quality
-            checks or transformations post extraction.
-          </li>
-        </ul>
-      </div>
-    </div>
+    <Card>
+      <Card.Img
+        variant="top"
+        src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
+      />
+      <Card.Body>
+        <Card.Title>Label Creation</Card.Title>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>
+          Take your cursor to the upper left corner of the relevant datapoint,
+          and drag the cursor to select the required portion.
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Assign labels to each field or table. Feel free to add any optional
+          comments to specify special processing needs, such as data quality
+          checks or transformations post extraction.
+        </ListGroup.Item>
+      </ListGroup>
+    </Card>
   );
 }
 function GeneralInstructions() {
   return (
-    <ul className="ollist">
-      <li className=" m-0 p-0">
-        For demo versions, model preparation is swift - generally completed in
-        under 4 hours. Our customer support team will keep you in the loop and
-        inform you the moment your model is ready.
-      </li>
-      <li className=" m-0 p-0">
-        Once your model is trained, it's all set to efficiently process bulk
-        quantities of similar documents. Efficient, effective, and tailored just
-        for you!
-      </li>
-    </ul>
+    <Card>
+      <Card.Body>
+        <Card.Title>Model Preparation</Card.Title>
+      </Card.Body>
+      <ListGroup className="list-group-flush">
+        <ListGroup.Item>
+          For demo versions, model preparation is swift - generally completed in
+          under 4 hours. Our customer support team will keep you in the loop and
+          inform you the moment your model is ready.
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Once your model is trained, it's all set to efficiently process bulk
+          quantities of similar documents. Efficient, effective, and tailored
+          just for you!
+        </ListGroup.Item>
+      </ListGroup>
+    </Card>
   );
 }
 
