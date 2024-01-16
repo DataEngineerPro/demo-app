feaaa5a73c5fcf59f3b6bc1724700a33c2fe6397
Kuldeep Desai <kuldeep.desai@qumu.com>
2024-01-16 21:51:54 +0100

    intro gif

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -53,10 +53,7 @@ function IntroComponent(props: any) {
 function CanvasIntroComponent() {
   return (
     <Card>
-      <Card.Img
-        variant="top"
-        src="https://user-images.githubusercontent.com/14011726/94132137-7d4fc100-fe7c-11ea-8512-69f90cb65e48.gif"
-      />
+      <Card.Img variant="top" src="/assets/label.gif" />
       <Card.Body>
         <Card.Title>Label Creation</Card.Title>
       </Card.Body>
